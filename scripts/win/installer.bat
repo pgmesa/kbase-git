8892f52a0371c396cb4e064a640e28b179a8af37
Pablo García Mesa <pablogmesa00@gmail.com>
2022-01-26 21:57:44 +0100

    Añadiendo installer en windows

---
+++ scripts/win/installer.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set batch_file=kbase-git.bat
+echo Instalando globalmente el archivo '%batch_file%'...
+call .global.bat --install %batch_file%
+@REM Puesto que despues de dr privilegios, se vuelve a ejecutar .global.bat, este archivo y .global se ejecutan
+@REM a la vez, por lo que si se ejecuta esto antes que el otro saldra una info erronea, por eso el timeout que solo
+@REM se puede romper con control-c
+timeout /t 1 /nobreak > nul
+if exist "C:\Windows\System32\%batch_file%" (
+    echo SUCCESS: Aplicacion instalada globalmente con exito 
+    echo -- introduce 'kbase-git' para iniciar la aplicacion --
+    goto success
+) else (
+    echo ERROR: Fallo al instalar globalmente el archivo '%batch_file%'
+    goto failure
+)
+
+:success
+    pause
+    exit /B 0
+
+:failure
+    pause
+    exit /B 1
---
+++ scripts/win/uninstaller.bat
@@ -0,0 +1,27 @@
+@echo off
+
+set batch_file=kbase-git.bat
+
+echo Eliminando global batch file '%batch_file%'...
+call .global.bat --uninstall %batch_file%
+@REM Puesto que despues de dr privilegios, se vuelve a ejecutar .global.bat, este archivo y .global se ejecutan
+@REM a la vez, por lo que si se ejecuta esto antes que el otro saldra una info erronea, por eso el timeout que solo
+@REM se puede romper con control-c
+timeout /t 1 /nobreak > nul
+@REM Poner parentesis en echo hace que el script llegue a dar error. Toma los parentesis como si fueran del script
+@REM y no parentesis como string (evitarlo)
+if exist "C:\Windows\System32\%batch_file%" (
+    echo ERROR: Fallo al desinstalar globalmente la aplicacion -- archivo '%batch_file%' --
+    goto failure
+) else (
+    echo SUCCESS: Aplicacion desinstalada globalmente con exito
+    goto success
+)
+
+:success
+    pause
+    exit /B 0
+
+:failure
+    pause
+    exit /B 1
