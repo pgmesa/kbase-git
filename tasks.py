fd54b3757b238c98dfa3384baa71364f0dac8c93
Pablo García Mesa <pablogmesa00@gmail.com>
2022-01-28 23:30:38 +0100

    Automated Tasks on Mac & Linux finished

--- tasks.py
+++ tasks.py
@@ -1,9 +1,12 @@
 
+import os
 import csv
+from pathlib import Path
 from subprocess import CalledProcessError, Popen, run, PIPE
 
 win_tasks_dir = "KbaseGitTasks"
 task_command = "kbase-git upload -g --counter"
+execution_path = Path(__file__).resolve().parent
 
 def create_task(OS:str, tasks:dict, override=False):
     if override:
@@ -21,8 +24,31 @@ def create_task(OS:str, tasks:dict, override=False):
                     f'/TR "{task_command}" /ST {time}')
             Popen(cmd, shell=True).wait()
     else:
-        ...
-    
+        extasks = get_tasks(OS)
+        if len(extasks) != 0:
+            show_tasks(OS)
+            answer = str(input("WARNING: There are already tasks scheduled on crontab. Override? (y/n): "))
+            if answer.lower() != 'y':
+                return
+        cronfile = ""
+        for task_name, time in tasks.items():
+            try:
+                check_input_time(time)
+            except Exception as err:
+                msg = (f" El tiempo introducido '{time}' no es correcto" + 
+                        f"\n     ERR MSG -> {err}")
+                print(msg)
+                continue
+            hours = int(time[:2]); mins = int(time[3:])
+            cron_task = f"{mins} {hours} * * * {task_command}"
+            cronfile += f"# {task_name}\n{cron_task}\n"
+        if cronfile != "":
+            cron_fpath = execution_path/'tempsh'
+            with open(cron_fpath, 'w') as file:
+                file.write(cronfile)
+            run(f'crontab {cron_fpath}', shell=True)
+            os.remove(cron_fpath)
+        
 def show_tasks(OS:str):
     print(" + Scheduled Tasks:")
     tasks = get_tasks(OS)
@@ -47,7 +73,7 @@ def remove_tasks(OS:str, force=False):
             cmd = (f'SCHTASKS /DELETE /TN "{task}" {f}')
             Popen(cmd, shell=True).wait()
     else:
-        run('crontab -r', shell=True)
+        run('crontab -r', shell=True, stdout=PIPE, stderr=PIPE)
         print("[%] Tasks removed")
         
 def get_tasks(OS:str) -> list:
@@ -60,8 +86,12 @@ def get_tasks(OS:str) -> list:
             if i == 0: continue
             tasks.append(row[0])
     else:
-        lines = run('crontab -l', shell=True, stdout=PIPE).stdout.decode().splitlines()
-        tasks = list(filter(lambda line: "#" not in line and line != "", lines))
+        try:
+            lines = run('crontab -l', shell=True, stdout=PIPE, stderr=PIPE).stdout.decode().splitlines()
+        except CalledProcessError:
+            pass
+        else:
+            tasks = list(filter(lambda line: "#" not in line and line != "", lines))
     
     return tasks
         
