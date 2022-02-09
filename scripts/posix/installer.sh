e926b7cb4b0b3e3a5a0ab7c8709cc79f9bb5cf10
pgmesa <pgmesa.sm@gmail.com>
2022-02-09 19:40:18 +0100

    adding msgs to posix scripts

--- scripts/posix/installer.sh
+++ scripts/posix/installer.sh
@@ -3,6 +3,7 @@
 python3 ./../.path.py --replace
 if [ $? -eq 0 ]; then
     sudo cp ./kbase-git.sh /usr/local/bin/kbase-git
+    echo [%] Instalation Succeeded
     python3 ./../.path.py --reset
 else
     echo [!] Instalation failed
--- scripts/posix/uninstaller.sh
+++ scripts/posix/uninstaller.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 sudo rm /usr/local/bin/kbase-git
+if [ $? -eq 0 ]; then
+    echo [%] Uninstalation Succeeded
+else
+    echo [!] Uninstalation Failed
+    exit 1
+fi
