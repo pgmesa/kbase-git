c5f4fccf2e9ae793c131daf0789f1ac636a908c0
pgmesa <pgmesa.sm@gmail.com>
2022-02-09 10:30:02 +0100

    Changing script for MacOS

--- scripts/posix/kbase-git.sh
+++ scripts/posix/kbase-git.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-echo Running keybase service...
-run_keybase 1> /dev/null
-sleep 2s
-if [ $? -eq 0 ]; then
-    code_path=#PATH#
+code_path=#PATH#
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # MacOS
     python3 "$code_path/kbase-git.py" $@
 else
-    echo [!] Could not start keybase service, is keybase installed?
-    exit 1
+    # Linux
+    echo Running keybase service...
+    run_keybase 1> /dev/null
+    if [ $? -eq 0 ]; then
+        sleep 2s
+        python3 "$code_path/kbase-git.py" $@
+    else
+        echo [!] Could not start keybase service, is keybase installed?
+        exit 1
+    fi
 fi
